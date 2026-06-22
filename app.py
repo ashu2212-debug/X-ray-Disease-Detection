6d82c493732d3b3dc0f933b5ad8e456f3fcae834
Ashutosh Kumar pandey <ashutoshmrj.164@gmail.com>
2026-06-23 00:13:56 +0530

    Implement model download and confidence display
    Added functionality to download the model if not present and display confidence of prediction.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 from PIL import Image
+import urllib.request
+import os
+
+# Download model if not present
+MODEL_URL = "https://huggingface.co/ashu3324234/x-ray-disease-model/resolve/main/xray_disease_model.h5"
+MODEL_PATH = "xray_disease_model.h5"
+
+if not os.path.exists(MODEL_PATH):
+    with st.spinner("Downloading model... Please wait"):
+        urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
 
 # Load Model
-model = load_model("xray_disease_model.h5")
+model = load_model(MODEL_PATH)
 
 classes = [
     "COVID19",
@@ -27,7 +36,7 @@ if uploaded_file is not None:
 
     st.image(img, caption="Uploaded Image", width=300)
 
-    img = img.resize((224,224))
+    img = img.resize((224, 224))
 
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
